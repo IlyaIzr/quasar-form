79cdb089489a622ebac297faf15e366ffd1fd9b2
ilyaizr <ilyaizrailyan@gmail.com>
2020-10-30 21:51:20 +0300

    WS fixes

--- src/fakeData.js
+++ src/fakeData.js
@@ -8,6 +8,7 @@ const db_data_group_options = [
 export const config = {
   data: {
     fields: [
+      //1
       {
         type: 'select', rowIndex: 1, key: 'sel1base', // required properties
         label: '@(Группа)',
@@ -15,20 +16,27 @@ export const config = {
         value: { name: 'Beta', id: '2aaaZ' },
         onChange: () => {
           const updater = async (vNode, vNodeStore) => {
-            const { value, rest, options } = vNode
+            const { value, rest } = vNode
 
             //Request
-            await WS.connectionEstablished()
-            const { status, data } = await WS.send('transactions2', 'usersDataSenders', value)
+            const { status, data, options } = await WS.send('transactions2', 'usersDataSenders', value)
             if (status === 'OK') {
               const vTarget = vNodeStore.getComponent('sendersKey132')
               vTarget.onInput(data)
               vTarget.setOptions(options)
             }
+
+            const response = await WS.send('transactions2', 'usersDataRecipients', value)
+            if (response.status === 'OK') {
+              const vTarget = vNodeStore.getComponent('recieverkey214')
+              vTarget.onInput(response.data)
+              vTarget.setOptions(response.options)
+            }
           }
           return updater
         }
       },
+      //2
       {
         type: 'select', rowIndex: 2, key: 'sendersKey132', // required
         label: '@(Отправитель)',
@@ -38,7 +46,6 @@ export const config = {
             const { value, rest } = vNode
 
             // Request
-            await WS.connectionEstablished()
             const { status, data, options } = await WS.send('transactions2', 'accountsDataSender', value)
             if (status === 'OK') {
               const vTarget = vNodeStore.getComponent('accountsKey132')
@@ -49,8 +56,36 @@ export const config = {
           return updater
         }
       },
+      //3
       {
         type: 'select', rowIndex: 2, key: 'accountsKey132', // required
+        label: '@(Счёт отправителя)',
+        options: []
+      },
+      //4
+
+      {
+        type: 'select', rowIndex: 3, key: 'recieverkey214', // required
+        label: '@(Отправитель)',
+        options: [],
+        onChange: () => {
+          const updater = async (vNode, vNodeStore) => {
+            const { value, rest } = vNode
+
+            // Request
+            const { status, data, options } = await WS.send('transactions2', 'accountsDataRecipients', value)
+            if (status === 'OK') {
+              const vTarget = vNodeStore.getComponent('accountsKey422')
+              vTarget.onInput(data)
+              vTarget.setOptions(options)
+            }
+          }
+          return updater
+        }
+      },
+      //5
+      {
+        type: 'select', rowIndex: 3, key: 'accountsKey422', // required
         label: '@(Счёт отправителя)',
         options: []
       }
@@ -61,9 +96,10 @@ export const config = {
       { type: 'submit', text: 'отправить' }
     ],
 
-    form: [
+    form: {
+      isModal: false,
       //form methods like onSubmit
-    ],
+    },
 
     title: 'Form title'
   },
