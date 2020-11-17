da401e047a76ce6643ba0520ca43a16b4774a91f
ilyaizr <ilyaizrailyan@gmail.com>
2020-11-17 19:39:42 +0300

    BUILD - html editor, default value bugfix

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,21 +1,5 @@
 export const config = {
   fields: [
-    {
-      type: 'editor', key: 'edt14',
-      value: '<b>B</b>eniss',
-      placeholder: 'Type here',
-      readonly: false,
-      disable: false,
-      minHeight: '150px',
-      maxHeight: '60vh',
-      height: '100%',
-      toolbar: [
-        ['left', 'center', 'right', 'justify'],
-        ['bold', 'italic', 'underline', 'strike'],
-        ['undo', 'redo'],
-      ]
-      // Quasar methods https://quasar.dev/vue-components/editor#QEditor-API
-    }
   ],
 
 
