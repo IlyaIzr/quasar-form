882425926e3dc0848b80b0473f026da149b945d4
ilyaizr <ilyaizrailyan@gmail.com>
2020-12-04 15:36:20 +0300

    multivalues tested - all Ok, and smaller clear icon

--- src/components/FieldMapper.vue
+++ src/components/FieldMapper.vue
@@ -170,9 +170,6 @@ export default {
       return res;
     },
   },
-  mounted() {
-    console.log(this.wrappedFields);
-  },
 };
 </script>
 
--- src/fakeData.js
+++ src/fakeData.js
@@ -1,13 +1,23 @@
 export const config = {
   fields: [
+    //Multiple fields
     {
-      type: 'multiple', key: 'multiKey', rowIndex: 4,
+      type: 'multiple', key: 'multi21', rowIndex: 4,
       value: [
-        { 'field1key': 'Peter',  },
-        { 'field1key': 'Lois',  }
+        { 'field1key': 'Peter', select12: 'option2', dateKey: '2012/12/12' },
+        { 'field1key': 'Lois', select12: 'option1', dateKey: '2020/12/12' }
       ],
       fields: [
         { type: 'text', rowIndex: 1, key: 'field1key', label: 'Name' },
+        {
+          type: 'select', rowIndex: 2, key: 'select12', label: 'Choose',
+          options: [
+            { id: 'option1', name: 'Your kids go to college' },
+            { id: 'option2', name: 'Batman glass' },
+            { id: 'option3', name: 'Meg' }
+          ]
+        },
+        { type: 'date', rowIndex: 1, key: 'dateKey', label: 'When?' }
       ],
       label: 'Multy fields!',
       buttons: {
@@ -15,8 +25,10 @@ export const config = {
         deleteField: { text: 'x', color: 'red', textColor: 'black', disabled: false }
       },
       shouldAddPreviousValue: false,
+      // defaultAddValues: {} //TBD,
       rules: [val => val >= 2 || 'Select at least 2 fields']
     },
+
   ],
 
   tabs: {
