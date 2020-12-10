5d491909d1596dfca423a96b195d4db7b1c29311
ilyaizr <ilyaizrailyan@gmail.com>
2020-12-10 12:33:49 +0300

    quasar version upgrade

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,40 +1,21 @@
 export const config = {
   fields: [
-    //Multiple fields
     {
-      type: 'multiple', key: 'multi21', rowIndex: 4,
-      value: [
-        { 'field1key': 'Peter', select12: 'option2',  },
-        // { 'field1key': '', select12: '',  }
-      ],
-      fields: [
-        { type: 'text', rowIndex: 1, key: 'field1key', label: 'Name', service: false, },
-        {
-          type: 'select', rowIndex: 2, key: 'select12', label: 'Choose', service: true,
-          options: [
-            { id: 'option1', name: 'Your kids go to college' },
-            { id: 'option2', name: 'Batman glass' },
-            { id: 'option3', name: 'Meg' }
-          ]
-        },
-        { type: 'date', rowIndex: 1, key: 'dateKey', label: 'When?', withInput: true, range: true }
-      ],
-      label: 'Multy fields!',
-      buttons: {
-        addField: { text: 'add', color: 'green', textColor: 'white' },
-        deleteField: { text: 'x', color: 'red', textColor: 'black', disabled: false }
-      },
-      shouldAddPreviousValue: false,
-      // defaultAddValues: {} //TBD,
-      rules: [val => val >= 1 || 'Select at least 1 field']
+      rowIndex: 1
     },
     {
-      key: 'testo', service: true, value: 'bob'
+      rowIndex: 1
+    },
+    {
+      key: 'testo', service: true, value: 'bob', rowIndex: 2
+    },
+    {
+      label: '3', rowIndex: 3
     }
 
   ],
 
-  
+  wrap: [[1, 2]],
 
   form: {
     async onSubmit(vNode, data, formMethods, vNodeStore) {
