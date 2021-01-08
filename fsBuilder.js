236c545c0bc46b520278ec1fd745dd22636b68c0
ilyaizr <ilyaizrailyan@gmail.com>
2021-01-08 15:19:34 +0300

    BUILD - css extracted by build script

--- fsBuilder.js
+++ fsBuilder.js
@@ -1,10 +1,11 @@
 var fs = require('fs')
-console.log('writing into lib')
+console.log('writing into /library')
 // Update vendor
 const vendor = fs.readdirSync('./dist/spa/js').filter(fn => fn.startsWith('vendor'));
 fs.readFile('./dist/spa/js/' + vendor[0], 'utf8', function (err, data) {
+  const replaced = data.replace(/webpackJsonp/g, 'webpackJsonpFB')
 
-  fs.writeFile('./library/vendor.js', data, 'utf8', function (err) {
+  fs.writeFile('./library/vendor.js', replaced, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
@@ -17,6 +18,7 @@ appMinified = appMinified.replace(/{globalConfig:[A-za-z]/g, '{globalConfig:form
 appMinified = appMinified.replace(/,globalValues:[A-za-z]/g, ',globalValues:formValues')
 appMinified = appMinified.replace(/"#q-app"/g, '"#"+rootId')
 appMinified = appMinified.replace(/"q-app"/g, 'rootId')
+appMinified = appMinified.replace(/webpackJsonp/g, 'webpackJsonpFB')
 
 
 const formBuilderContent =
@@ -26,6 +28,24 @@ const formBuilderContent =
 
 fs.readFile('./library/formBuilder.js', 'utf8', function (err, data) {
   fs.writeFile('./library/formBuilder.js', formBuilderContent, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+
+// Update css vendor
+const cssVendor = fs.readdirSync('./dist/spa/css').filter(fn => fn.startsWith('vendor'));
+fs.readFile('./dist/spa/css/' + cssVendor[0], 'utf8', function (err, data) {
+  fs.writeFile('./library/css/vendor.css', data, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+// Update app css
+const cssApp = fs.readdirSync('./dist/spa/css').filter(fn => fn.startsWith('app'));
+fs.readFile('./dist/spa/css/' + cssApp[0], 'utf8', function (err, data) {
+
+  fs.writeFile('./library/css/app.css', data, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
